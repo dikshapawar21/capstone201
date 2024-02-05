05c437a483fc62600628dddd09091e17600c4f39
Tarang Patil <tarangpatil09@gmail.com>
2024-02-05 23:59:30 +0530

    Add csrf verification

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require("express");
+const csurf = require("tiny-csrf");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 const app = express();
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser("cookie-parser-secret"));
+app.use(session({ secret: "keyboard cat" }));
+app.use(csurf("123456789iamasecret987654321look"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login", { csrfToken: req.csrfToken() });
+});
+
+app.get("/educatorSignup", (req, res) => {
+  res.render("educatorSignup", { csrfToken: req.csrfToken() });
+});
+
+app.get("/studentSignup", (req, res) => {
+  res.render("studentSignup", { csrfToken: req.csrfToken() });
 });
 
 module.exports = app;
